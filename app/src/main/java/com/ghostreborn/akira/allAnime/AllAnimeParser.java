e3fdc6d20c7f132b3e5e59afa6f7bed10701184d
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2024-07-19 21:31:49 +0530

    Condensed Code

--- app/src/main/java/com/ghostreborn/akira/allAnime/AllAnimeParser.java
+++ app/src/main/java/com/ghostreborn/akira/allAnime/AllAnimeParser.java
@@ -15,49 +15,38 @@ public class AllAnimeParser {
 
     public static List<Anime> queryPopular() {
         List<Anime> animes = new ArrayList<>();
-        String rawJSON = AllAnimeNetwork.queryPopular();
         try {
-            JSONArray recommendation = new JSONObject(rawJSON)
+            JSONArray recommendations = new JSONObject(AllAnimeNetwork.queryPopular())
                     .getJSONObject("data")
                     .getJSONObject("queryPopular")
                     .getJSONArray("recommendations");
-            for (int i = 0; i < recommendation.length(); i++) {
-                JSONObject anime = recommendation.getJSONObject(i)
-                        .getJSONObject("anyCard");
-                String name = anime.getString("name");
-                String englishName = anime.getString("englishName");
-                if (!englishName.equals("null")){
-                    name = englishName;
-                }
-                String thumbnail = anime.getString("thumbnail");
-                animes.add(new Anime(name, thumbnail));
+
+            for (int i = 0; i < recommendations.length(); i++) {
+                JSONObject anime = recommendations.getJSONObject(i).getJSONObject("anyCard");
+                String name = anime.optString("englishName", anime.getString("name"));
+                animes.add(new Anime(name, anime.getString("thumbnail")));
             }
         } catch (JSONException e) {
-            Log.e("AllAnimeParser", "Error parsing JSON: " + e);
+            Log.e("AllAnimeParser", "Error parsing JSON: ", e);
         }
         return animes;
     }
 
     public static List<Anime> searchAnime(String animeName) {
         List<Anime> animes = new ArrayList<>();
-        String rawJSON = AllAnimeNetwork.searchAnime(animeName);
         try {
-            JSONArray recommendation = new JSONObject(rawJSON)
+            JSONArray recommendations = new JSONObject(AllAnimeNetwork.searchAnime(animeName))
                     .getJSONObject("data")
                     .getJSONObject("shows")
                     .getJSONArray("edges");
-            for (int i = 0; i < recommendation.length(); i++) {
-                JSONObject anime = recommendation.getJSONObject(i);
-                String name = anime.getString("name");
-                String englishName = anime.getString("englishName");
-                if (!englishName.equals("null")){
-                    name = englishName;
-                }
-                String thumbnail = anime.getString("thumbnail");
-                animes.add(new Anime(name, thumbnail));
+
+            for (int i = 0; i < recommendations.length(); i++) {
+                JSONObject anime = recommendations.getJSONObject(i);
+                String name = anime.optString("englishName", anime.getString("name"));
+                animes.add(new Anime(name, anime.getString("thumbnail")));
             }
         } catch (JSONException e) {
-            Log.e("AllAnimeParser", "Error parsing JSON: " + e);
+            Log.e("AllAnimeParser", "Error parsing JSON: ", e);
         }
         return animes;
     }
