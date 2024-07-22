42d465f6f201c8591d0df5b047e41cedb970a626
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2024-07-22 18:44:56 +0530

    Immersive episode playback

--- app/src/main/java/com/ghostreborn/akira/ui/PlayEpisodeActivity.java
+++ app/src/main/java/com/ghostreborn/akira/ui/PlayEpisodeActivity.java
@@ -2,6 +2,7 @@ package com.ghostreborn.akira.ui;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -24,5 +25,17 @@ public class PlayEpisodeActivity extends AppCompatActivity {
         videoView.setMediaController(mediaController);
         videoView.start();
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
