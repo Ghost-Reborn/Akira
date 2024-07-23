acd8f84974013bcefa17a17852b96de5f2613654
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2024-07-24 05:04:37 +0530

    Anime Manga Bottom Navigation

--- app/src/main/java/com/ghostreborn/akira/MainActivity.java
+++ app/src/main/java/com/ghostreborn/akira/MainActivity.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ghostreborn.akira.fragment.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.home_fragment_container, new HomeFragment())
+                .commit();
+
     }
 }
